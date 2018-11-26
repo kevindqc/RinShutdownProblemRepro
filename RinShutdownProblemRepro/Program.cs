a5c82f4c85803f55dad9defbb07868126931aa50
Kevin Doyon <kdoyon@replaceservices.com>
2018-11-26 09:54:08 -0500

    Add Autofac

--- RinShutdownProblemRepro/Program.cs
+++ RinShutdownProblemRepro/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Autofac.Extensions.DependencyInjection;
 
 namespace RinShutdownProblemRepro
 {
@@ -19,6 +20,7 @@ namespace RinShutdownProblemRepro
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureServices(services => services.AddAutofac())
                 .UseStartup<Startup>();
     }
 }
